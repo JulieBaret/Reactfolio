51421d227db39bbc1ca5a748f94bea93a59f255b
Julie <jem.baret@gmail.com>
2023-09-01 14:42:23 +0200

    Install Emotion lib + Refacto UpToTop as a component ScrollToTheTop with a scrollPosition props and a children ArrowIcon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
-import UpToTop from './components/UpToTop/UpToTop'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Bio from './components/Bio/Bio'
 import Header from './components/Header/Header'
 import Timeline from './components/Timeline/Timeline'
 import './App.css'
+import ScrollToTheTop from './components/ScrollToTheTop/ScrollToTheTop'
+import ArrowIcon from './components/Icons/ArrowIcon'
 
 const App = () => {
   return (
@@ -23,7 +24,9 @@ const App = () => {
         <Footer />
       </main>
 
-      <UpToTop />
+      <ScrollToTheTop scrollPosition={1500}>
+        <ArrowIcon />
+      </ScrollToTheTop>
     </div>
   )
 }
---
+++ src/components/Icons/ArrowIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ArrowIcon() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" className='w-8 h-8 text-center shadow-md animate-pulse stroke-white bg-pink px-1 py-1 rounded-full'>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+        </svg>
+    );
+}
+
+export default ArrowIcon;
