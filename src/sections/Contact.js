2615c5656d1429320bfc062d5bb7c66a1e440ed8
Julie <jem.baret@gmail.com>
2023-09-02 12:21:11 +0200

    Change contact buttons direction + remove useless comment in FilledButton component

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
       <div className='2xl:mt-6 2xl:mb-6 flex flex-col justify-between h-[90vh]'>
         <div className='text-4xl font-extrabold text-red lg:text-5xl 2xl:text-6xl'>Let's keep <span class="underline underline-offset-3 decoration-8 decoration-red">in touch</span></div>
         <div className="flex flex-col items-center justify-center h-full">
-          <div className="flex md:flex-row flex-col items-center gap-3">
+          <div className="flex flex-col items-center gap-4">
             <UnfilledButton title="Email Me" url={`mailto:${contact.email}`} color="red" transitionColor="middle-pink" gradient={true} />
             <FilledButton title="Linkedin" color="blue" url={social.linkedin} filledLogo={true} />
             <FilledButton title="Github" color="black" url={social.github} />
