1f03342a1868d835f6190c1a0c19a2fcc4f0af97
Julie <jem.baret@gmail.com>
2023-09-02 12:06:38 +0200

    Fix ArrowList component with task className

--- src/components/ArrowList.js
+++ src/components/ArrowList.js
@@ -3,7 +3,7 @@ import uniqid from 'uniqid'
 const ArrowList = ({ task }) => {
     return (
         <li key={uniqid()}>
-            <p className='timeline-what'>› {task}</p>
+            <p className='text-dark-grey mt-2 text-sm 2xl:text-base'>› {task}</p>
         </li>
     );
 };
