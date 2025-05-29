b1e5a1a6492ca9a8013a6dc2b01953d6991c3d39
Julie <jem.baret@gmail.com>
2025-05-29 18:40:09 +0200

    chore: change favicon for robot emoji

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import { render } from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
