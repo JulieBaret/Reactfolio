03874923a0fbc3111d8e1d35088c179710b33277
Julie <jem.baret@gmail.com>
2025-05-29 18:31:42 +0200

    feat: change sections order

--- src/layouts/Body.js
+++ src/layouts/Body.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import About from '../sections/About';
-import Projects from '../sections/Projects';
-import Contact from '../sections/Contact';
-import Biography from '../sections/Biography';
-import Chronology from '../sections/Chronology';
+import React from "react";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+import Biography from "../sections/Biography";
+import Chronology from "../sections/Chronology";
 
 const Body = () => {
-    return (
-        <>
-        <About />
-        <Projects />
-        <Biography />
-        <Chronology />
-        <Contact />
-        </>
-    );
+  return (
+    <>
+      <About />
+      <Biography />
+      <Chronology />
+      <Projects />
+      <Contact />
+    </>
+  );
 };
 
 export default Body;
